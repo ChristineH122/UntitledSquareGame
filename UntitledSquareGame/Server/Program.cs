5d598c277fd47c1619f42ea9bf087cbeb2f82bbf
PaulAbloescher <abloescherpaul@gmail.com>
2019-11-25 12:09:32 +0100

    Added hello world server

--- UntitledSquareGame/Server/Program.cs
+++ UntitledSquareGame/Server/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
 
 namespace Server
 {
@@ -10,6 +12,17 @@ namespace Server
     {
         static void Main(string[] args)
         {
+            var listener = new TcpListener(new IPEndPoint(IPAddress.Any, 5050));
+            listener.Start();
+            var client = listener.AcceptTcpClient();
+
+            var buffer = new byte[1024];
+            var stream = client.GetStream();
+            stream.Read(buffer, 0, 1024);
+
+            var message = Encoding.ASCII.GetString(buffer);
+            Console.WriteLine(message);
+            Console.ReadKey();
         }
     }
 }
