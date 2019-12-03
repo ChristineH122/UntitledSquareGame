a4df1d7e1f0e9d6a1e9f14e4b7e02dc87807acec
Christian Kohout <christian.kohout@gmx.at>
2019-12-03 12:35:00 +0100

    Even better game over

--- UntitledSquareGame/Client/ViewModel/GameVM.cs
+++ UntitledSquareGame/Client/ViewModel/GameVM.cs
@@ -28,7 +28,7 @@ namespace Client.ViewModel
         private int playerTwoLives;
         private int score;
         private bool gameOver;
-        private bool showGameOverMessage;
+        private bool hideGameOverMessage;
         private ObservableCollection<Square> gameObjects;
 
         public GameVM()
@@ -39,6 +39,7 @@ namespace Client.ViewModel
             this.ConHandler.GameStateReceived += ConHandler_GameStateReceived;
             this.Square = new SquareVM(new Square(500, 250, 40, 40, GameObjectType.Player1));
             this.SecondPlayerSquare = new SquareVM(new Square(500, 350, 40, 40, GameObjectType.Player2));
+            this.HideGameOverMessage = true;
             //this.connectionHandler = new ConnectionHandler("192.168.178.20", 5050);
         }
 
@@ -143,15 +144,15 @@ namespace Client.ViewModel
             }
         }
 
-        public bool ShowGameOverMessage
+        public bool HideGameOverMessage
         {
             get
             {
-                return this.showGameOverMessage;
+                return this.hideGameOverMessage;
             }
             set
             {
-                this.showGameOverMessage = value;
+                this.hideGameOverMessage = value;
                 this.FireOnPropertyChanged();
             }
         }
@@ -261,7 +262,7 @@ namespace Client.ViewModel
             this.PlayerTwoLives = e.GameState.PlayerTwoLives;
             this.Score = e.GameState.Score;
             this.GameOver = e.GameState.GameOver;
-            this.ShowGameOverMessage = !e.GameState.GameOver;
+            this.HideGameOverMessage = !e.GameState.GameOver;
         }
     }
 }
